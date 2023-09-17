7043055a25552de15560bc86acd022a7614f013e
aklima12 <128598790+aklima12@users.noreply.github.com>
2023-09-16 23:05:04 -0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,16 @@
+function styleBtn(){
+var fbBtn=document.querySelector('.fbBtn');
+fbBtn.id='demo';
+
+
+
+}
+function addSrc(){
+    var img=document.querySelector('img');
+    img.src='images (2).jfif';
+   
+}
+
+
+
+// const cars=["BMW","Volvo","Saad","Ford","Fiat"]
